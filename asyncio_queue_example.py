4e6059f1f4475bb7e8253433ae1259f84cf22d78
happymix <happymix2555@gmail.com>
2023-08-21 05:41:59 +0700

    update queue example

--- asyncio_queue_example.py
+++ asyncio_queue_example.py
@@ -5,7 +5,14 @@ import time
 '''
 	This program is used to demonstrate how to use asyncio.Queue
 	to share data between coroutine in producer-consumer manner.
+
+	In this example we're going to use multiple consumer task
 '''
+
+QueueSizeInt = 1
+NumberOfItemToPutInQueueInt = 2
+NumberOfConsumerInt = 2
+LongRunningProcessTimeSec = 2
 
 # producer function
 async def producer( queue ):
@@ -15,23 +22,25 @@ async def producer( queue ):
 		ARG: queue ( asyncio.Queue )
 	'''
 
-	# loop 3 times
-	for itemInt in range( 3 ):
+	# loop to create item in queue according to defined parameter
+	for itemInt in range( NumberOfItemToPutInQueueInt ):
 
 		# perform some operation with item
 		itemAfterSomeOperation = itemInt + 10
 
 		print( 'Done performing some operation with result = {}'.format( itemAfterSomeOperation ) )
 
-		print( 'putting item into queue' )
+		print( 'try putting item {} into queue'.format( itemAfterSomeOperation ) )
 
 		# putting item into queue
 		await queue.put( itemAfterSomeOperation )
 
-		print(f'Item: { itemAfterSomeOperation } has been put into queue!')
+		print( 'Item: {} has been put into queue!'.format( itemAfterSomeOperation ) )
+	
+	print( 'All Item has been processed so stopping the producer now' )
 
 # consumer function
-async def consumer( queue ):
+async def consumer( queue, consumerNameStr ):
 	''' This function will consume item from queue to perform some processing
 
 		ARG: queue ( asyncio.Queue )
@@ -40,21 +49,19 @@ async def consumer( queue ):
 	# loop forever
 	while True:
 
-		print( 'Getting item from queue' )
+		print( 'Consumer {} is getting item from queue'.format( consumerNameStr ) )
 
 		# get item from queue
 		itemInt = await queue.get()
 
-		# signal to stop this program
-		if itemInt is None:
-			break
+		print( 'Consumer {} got item {} from queue'.format( consumerNameStr, itemInt ) )
 		
-		print( 'Performing some long running process...' )
+		print( 'Consumer {}, Performing some long running process of item {}'.format( consumerNameStr, itemInt ) )
 
 		# simulate long running process
-		await asyncio.sleep( 5 )
+		await asyncio.sleep( LongRunningProcessTimeSec )
 
-		print( 'The long running process in consumer was done!' )
+		print( 'Consumer {}, The long running process of item {} in consumer was done!'.format( consumerNameStr, itemInt ) )
 
 		# tell asyncio.Queue that formerly enqueued task is done
 		# if we use queue.join() method
@@ -65,26 +72,46 @@ async def consumer( queue ):
 async def main():
 
 	# create instance of asyncio.Queue with maximum queue size = 1 
-	queue = asyncio.Queue( maxsize=2 )
+	queue = asyncio.Queue( maxsize=QueueSizeInt )
 
 	# create producer task
 	producerTask = asyncio.create_task( producer( queue ), name='producer task' )
 
-	# creat consumer task
-	consumerTask = asyncio.create_task( consumer( queue ), name='consumer task' )
+	# consumer task storage 
+	consumerTaskList = list()
+
+	# loop 5 time
+	for consumerCount in range( NumberOfConsumerInt ):
+
+		# creat consumer task with name
+		# and store it
+		consumerTaskList.append( asyncio.create_task( consumer( queue, str( consumerCount ) ), name='consumer_task_{}'.format( consumerCount ) ) )
 
 	print( 'We have created producer and consumer task and scheduled it to run in eventloop...' )
 
-	# yield control to the eventloop to run tasks in it for sometime
-	await asyncio.sleep(10)
+	# yield control of our program to eventloop and 
+	# wait for producer task to finish 
+	await producerTask
 
-	# signal consumer to stop
-	await queue.put( None )
+	# wait for all item in queue to be processed
+	await queue.join()
 
-	# wait for both producer and consumer to finish 
-	await asyncio.gather( producerTask, consumerTask )
+	# loop through each consumer task
+	for consumerTask in consumerTaskList:
+
+		# cancel consumer task	
+		consumerTask.cancel()
+	
+	# wait until all worker tasks are cancelled
+	await asyncio.gather( *consumerTaskList, return_exceptions=True )
 
 	print( 'Exiting...' )
 
+# program start time 
+startTime = time.time()
+
 # run our program
 asyncio.run( main() )
+
+# time used for the whole program
+print( 'Time used for the whole program was: {}s'.format( time.time() - startTime ) )
